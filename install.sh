9b2fd7247da0ed2f9a2a075e4e49a3ca1a4afd8c
Mahmud Ahmad <eeternalspring@github.com>
2022-03-14 22:59:19 -0400

    add install script (wip)

---
+++ install.sh
@@ -0,0 +1,21 @@
+# step one, check if zsh is installed
+which zsh
+zsh_installed=$?
+
+if [ $zsh_installed -ne 0 ]
+then
+  # if not raise an error
+  echo 'Please install ZSH before continuing!'
+  exit 1
+fi
+
+echo 'Setting ZSH as main shell...'
+chsh -s $(which zsh)
+
+echo 'ZSH is main shell...'
+
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
