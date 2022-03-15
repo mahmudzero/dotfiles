0e3049fbc87f01e76e3fea6436a1ea2d182753f9
Mahmud Ahmad <eeternalspring@github.com>
2022-03-14 23:03:40 -0400

    ln dotfile to $HOME

--- install.sh
+++ install.sh
@@ -19,3 +19,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 # install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+ln -s ./zshrc ~/.zshrc
+ln -s ./vim ~/.vim
+ln p10k.zsh ~/.p10k.zsh
