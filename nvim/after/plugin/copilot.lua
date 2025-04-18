fbd18c0853266e9da7dc995cb9936a64c93a4ddf
Mahmud Ahmad <duelwith_mahmud@live.com>
2025-04-17 23:12:38 -0400

    copilot shift-tab

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -2,6 +2,8 @@ local handle = io.popen('pwd')
 local current_dir = handle:read('*a')
 handle:close()
 
+vim.api.nvim_set_keymap('i', '<S-Tab>', 'copilot#Accept("<CR>")', { expr = true, silent = true })
+
 vim.b.copilot_enabled = false
 if string.match(current_dir, "/Users/mahmudahmad/work") or string.match(current_dir, "/Users/mahmudahmad/Development/work") then
 	vim.b.copilot_enabled = true
