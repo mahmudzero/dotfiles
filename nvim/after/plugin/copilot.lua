b8fbf8f6674baa4dae658a01224861990bb984d1
Mahmud Ahmad <duelwith_mahmud@live.com>
2025-08-05 17:25:43 -0400

    need to use 1/0 not true/false

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -4,7 +4,9 @@ handle:close()
 
 vim.api.nvim_set_keymap('i', '<S-Tab>', 'copilot#Accept("<CR>")', { expr = true, silent = true })
 
-vim.b.copilot_enabled = false
+vim.b.copilot_enabled = 0
+vim.b.copilot_disabled = 1
 if string.match(current_dir, "/Users/mahmudahmad/work") or string.match(current_dir, "/Users/mahmudahmad/Development/work") then
-	vim.b.copilot_enabled = true
+	vim.b.copilot_enabled = 1
+	vim.b.copilot_disabled = 0
 end
